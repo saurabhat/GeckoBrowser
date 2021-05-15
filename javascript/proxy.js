910e12a294530410c84d03b89784ea6b3753349c
Nehaat <nehaat@exatetechnology.com>
2021-05-15 19:14:22 +0530

    first commit for Gecko Browser

---
+++ javascript/proxy.js
@@ -0,0 +1,14 @@
+﻿// do not need the <script> flag as it gets wraped around the code as a part of c#
+function sayHello()
+{
+	window.external.ShowMessage('hi in file')
+}
+
+function login() {
+	window.external.ShowMessage('hi in file')
+}
+
+
+function getPersona() {
+	alert(window.external.GetPersona());
+}
